e8c30fa1446d12aa3cf7a15a7537775caa59628d
coding-fantastic <alexnzioka@protonmail.com>
2022-03-07 16:21:51 +0300

    can work without importing pymysql only requiment is pip install mysqlclient

--- crudProject/settings.py
+++ crudProject/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
-import pymysql
-pymysql.install_as_MySQLdb()
+
+# import pymysql
+# pymysql.install_as_MySQLdb()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
